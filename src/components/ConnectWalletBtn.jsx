f8617fc2f41d954085af7031bfbc1eda0f354782
EmmanuelAKpan013 <sundayemma013@gmail.com>
2022-11-13 00:54:58 +0100

    Feat: Navbar Completion

---
+++ src/components/ConnectWalletBtn.jsx
@@ -0,0 +1,11 @@
+import "./ConnectWalletBtn.css";
+
+function ConnectWalletBtn() {
+  return (
+    <div>
+      <button className="connect-wallet">Connect wallet</button>
+    </div>
+  );
+}
+
+export default ConnectWalletBtn;
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,28 @@
 import "./NavBar.css";
+import metaLogo from "../images/metabnbLogo.png";
+import ConnectWalletBtn from "./ConnectWalletBtn";
 
 import React from "react";
 
 function NavBar() {
-  return <div>NavBar</div>;
+  return (
+    <div className="navbar-container">
+      <div className="navbar-logo-container">
+        <img src={metaLogo} alt="" />
+      </div>
+      <div className="navbar-list">
+        <ul>
+          <li>Home</li>
+          <li>Place to stay</li>
+          <li>NFTs</li>
+          <li>Community</li>
+        </ul>
+      </div>
+      <div className="connect-to-wallet">
+        <ConnectWalletBtn />
+      </div>
+    </div>
+  );
 }
 
 export default NavBar;
