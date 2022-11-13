4ad785f95ef36287c9953953d9999ef7d3c1192e
EmmanuelAKpan013 <sundayemma013@gmail.com>
2022-11-13 22:07:06 +0100

    Feat: Homepage Update

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,27 +1,31 @@
 import "./Card.css";
 import data from "../data/data.json";
-import testImg from "../images/test.png";
-import starImg from "../images/star.png";
 
 function Card() {
   return (
     <>
-      <div className="card-container">
-        <div className="card-image">
-          <img src={testImg} alt="nft--img" />
-        </div>
-        <div className="card-details">
-          <p>Desert King</p>
-          <p>1MBT per night</p>
-        </div>
-        <div className="card-details">
-          <p>2345km away</p>
-          <p>available for 2weeks stay</p>
-        </div>
-        <div className="star-container">
-          <img src={starImg} alt="" />
-        </div>
-      </div>
+      {data.map((item) => {
+        return (
+          <div className="card-container">
+            <div className="card-image">
+              <img src={item.image} alt="nft--img" />
+            </div>
+            <div className="card-details">
+              <p>{item.name}</p>
+              <p className="night">
+                <b>{item.quantity}</b>
+              </p>
+            </div>
+            <div className="card-details">
+              <p>{item.distance}</p>
+              <p>{item.availability}</p>
+            </div>
+            <div className="star-container">
+              <img src={item.rating} alt="" />
+            </div>
+          </div>
+        );
+      })}
     </>
   );
 }
