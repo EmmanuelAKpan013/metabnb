3fdaa3c10ca05b4d6976d0d584912444ab960d82
EmmanuelAKpan013 <sundayemma013@gmail.com>
2022-11-13 01:20:37 +0100

    Feat: Hero Section

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,7 +4,26 @@ import NavBar from "../components/NavBar";
 function HomePage() {
   return (
     <>
-      <NavBar />
+      <div>
+        <NavBar />
+      </div>
+      <div className="homepage-hero">
+        <div>
+          <div className="hero-rent-text">
+            <p className="homepage-hero-rent">
+              Rent a <span className="purple-text">Place</span> away from
+              <span className="purple-text"> Home</span> in the
+              <span className="purple-text"> Metaverse</span>
+            </p>
+            <p>
+              we provide you access to luxury and affordable houses in the
+              metaverse, get a chance to turn your imagination to reality at
+              your comfort zone
+            </p>
+          </div>
+        </div>
+        <div></div>
+      </div>
     </>
   );
 }
