1e209cf93b24cdacb574049b6b3494644bd0d8b0
EmmanuelAKpan013 <sundayemma013@gmail.com>
2022-11-14 22:04:56 +0100

    Update

--- src/pages/PlaceToStayPage.jsx
+++ src/pages/PlaceToStayPage.jsx
@@ -1,9 +1,36 @@
 import "./PlaceToStayPage.css";
-
-import React from "react";
+import NavBar from "../components/NavBar";
+import filterIcon from "../images/placetostay/filterIcon.svg";
 
 function PlaceToStayPage() {
-  return <div>PlaceToStayPage</div>;
+  return (
+    <>
+      <div className="place-nav-container">
+        <NavBar />
+      </div>
+      <div className="list-container">
+        <div className="listings">
+          <ul className="short-list">
+            <li>Resturant</li>
+            <li>Cottage</li>
+            <li>Castle</li>
+            <li>fantast city</li>
+            <li>beach</li>
+            <li>Carbins</li>
+            <li>Off-grid</li>
+            <li>Farm</li>
+            <li>
+              <div className="list-location">
+                <p>Location</p>
+                <img src={filterIcon} alt="" />
+                <div></div>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default PlaceToStayPage;
